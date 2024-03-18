9681f1d83e9b1c33b4204303fae0ef968b02efc3
Artem S <saltem@inbox.ru>
2024-03-18 14:47:12 +0300

    Добавление дополнительного "Hello World"

--- src/main/java/com/stepup/project_dz/MainApplication.java
+++ src/main/java/com/stepup/project_dz/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepup.project_dz;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello World");
     }
 }
